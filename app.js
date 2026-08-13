cd7c9e99c7da50962941553fa7c075a37c005c11
Mohamed Ghareeb <mohamedghareeb20@yahoo.com>
2026-08-13 16:57:15 +0300

    update CORS for production

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cookieParser());
 
-app.use(cors({origin:[ 'http://localhost:5173', "http://localhost:5174" ] , credentials: true}));
+app.use(cors({origin:[process.env.FRONTEND_URL, process.env.ADMIN_URL, 'http://localhost:5173', "http://localhost:5174" ] , credentials: true}));
 
 app.use("/images", express.static("uploads"));
 
