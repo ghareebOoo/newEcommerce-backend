6a8de5df57f5ff1a1e56bd60db1e5123ea4e7769
Mohamed Ghareeb <mohamedghareeb20@yahoo.com>
2026-08-13 20:31:52 +0300

    second fix app.js

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ import { stripeWebhook } from "./controllers/orderController.js";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.method, req.originalUrl);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(req.method, req.originalUrl);
+//   next();
+// });
 
 app.use(cookieParser());
 
